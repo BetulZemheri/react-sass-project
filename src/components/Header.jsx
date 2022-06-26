e84f85eaec95870194aad93450c8daff16de75c9
Betul Zemheri <a.betul2200@gmail.com>
2022-06-26 12:51:27 +0300

    Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import headerStyle from "../scss/header.module.scss";
+
+const Header = () => {
+    return (
+        <header className={headerStyle["title"]}>
+            COMMENTS
+        </header>
+    );
+};
+
+export default Header;
